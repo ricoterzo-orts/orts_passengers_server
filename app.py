d21b6a3efef5af3574e85516f1a8ec7997ff45e4
ricoterzo-orts <ricoterzo@gmail.com>
2026-06-13 13:46:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -941,7 +941,7 @@ def api_heartbeat():
         conn.commit()
 
     if new_session:
-        msg = f"🚆 **{user['username']}** è entrato in linea"
+        msg = f"🚆 **{user['username']}** è in servizio su"
         if activity_name:
             msg += f" su *{activity_name}*"
         notify_discord(embed={
